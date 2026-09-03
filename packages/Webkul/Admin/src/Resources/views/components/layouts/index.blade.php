47691343699bc5d3246f7e416a04df59b232a76d
329shankar-pixel <329shankar-pixel@users.noreply.github.com>
2026-09-03 17:28:50 +0545

    ArinaFits: remove Bagisto credit footer and Help resources nav
    - Drop Powered by Bagisto footer bar from admin layout
    - Drop Help & Resources entry from admin sidebar and mobile drawer

--- packages/Webkul/Admin/src/Resources/views/components/layouts/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/layouts/index.blade.php
@@ -119,16 +119,6 @@
                         {{ $slot }}
                     </div>
                 </div>
-
-                <!-- Powered By -->
-                <div class="mt-auto">
-                    <div class="border-t bg-white py-2 text-center text-xs sm:text-sm dark:border-gray-800 dark:bg-gray-900 dark:text-white">
-                        @lang('admin::app.components.layouts.powered-by.description', [
-                            'bagisto' => '<a class="text-arina-dark hover:underline dark:text-arina-light" href="https://bagisto.com/en/">Bagisto</a>',
-                            'webkul' => '<a class="text-arina-dark hover:underline dark:text-arina-light" href="https://webkul.com/">Webkul</a>',
-                        ])
-                    </div>
-                </div>
             </div>
         </div>
 
